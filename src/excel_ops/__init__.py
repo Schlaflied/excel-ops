465bd36282a7f899d285f28c2fed34a4b9454f56
Schlaflied <200203syttt@gmail.com>
2026-09-12 05:03:57 -0400

    feat: add offline review pack round trip

--- src/excel_ops/__init__.py
+++ src/excel_ops/__init__.py
@@ -1,5 +1,23 @@
 """Excel-Ops public package."""
 
 from .pipeline import run_pipeline
+from .review_pack import (
+    ReviewDecision,
+    ReviewHistoryEntry,
+    ReviewImportResult,
+    ReviewPackRow,
+    import_review_pack,
+    review_rows_from_match_results,
+    write_review_pack,
+)
 
-__all__ = ["run_pipeline"]
+__all__ = [
+    "ReviewDecision",
+    "ReviewHistoryEntry",
+    "ReviewImportResult",
+    "ReviewPackRow",
+    "import_review_pack",
+    "review_rows_from_match_results",
+    "run_pipeline",
+    "write_review_pack",
+]
