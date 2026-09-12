757c74e914ee80a06feb0bc4114914903093c512
Schlaflied <200203syttt@gmail.com>
2026-09-12 04:55:10 -0400

    feat: add strict record matching and review queue

--- src/excel_ops/__init__.py
+++ src/excel_ops/__init__.py
@@ -3,3 +3,12 @@
 from .pipeline import run_pipeline
 
 __all__ = ["run_pipeline"]
+from .matching import Destination, MatchCandidate, MatchResult, preallocate_records, stable_record_id
+
+__all__ = [
+    "Destination",
+    "MatchCandidate",
+    "MatchResult",
+    "preallocate_records",
+    "stable_record_id",
+]
